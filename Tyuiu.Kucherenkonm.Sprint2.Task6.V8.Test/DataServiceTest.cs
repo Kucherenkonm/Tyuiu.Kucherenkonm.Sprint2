f7a7006ecaeb0e2f641eebd10d9567c8bbf0cc3e
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-10-16 20:48:17 +0500

    Task6

---
+++ Tyuiu.Kucherenkonm.Sprint2.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.Kucherenkonm.Sprint2.Task6.V8.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint2.Task6.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay()
+        {
+            DataService ds = new DataService();
+            int m = 2;
+
+            Assert.AreEqual("01.01", ds.FindDateOfPreviousDay(m, 1));
+            Assert.AreEqual("01.02", ds.FindDateOfPreviousDay(m, 2));
+            Assert.AreEqual("01.03", ds.FindDateOfPreviousDay(m, 3));
+            Assert.AreEqual("01.04", ds.FindDateOfPreviousDay(m, 4));
+            Assert.AreEqual("01.05", ds.FindDateOfPreviousDay(m, 5));
+            Assert.AreEqual("01.06", ds.FindDateOfPreviousDay(m, 6));
+            Assert.AreEqual("01.07", ds.FindDateOfPreviousDay(m, 7));
+            Assert.AreEqual("01.08", ds.FindDateOfPreviousDay(m, 8));
+            Assert.AreEqual("01.09", ds.FindDateOfPreviousDay(m, 9));
+            Assert.AreEqual("01.10", ds.FindDateOfPreviousDay(m, 10));
+            Assert.AreEqual("01.11", ds.FindDateOfPreviousDay(m, 11));
+            Assert.AreEqual("01.12", ds.FindDateOfPreviousDay(m, 12));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDateOfPreviousDay(1, -1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDateOfPreviousDay(32, 13);
+            });
+        }
+        
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint2.Task6.V8/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.Kucherenkonm.Sprint2.Task6.V8.Lib;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема:              Получение результата из switch                       *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #8                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*    Написать программу, которая использует оператор switch вычисляет     *");
+        Console.WriteLine("*               требуемое значение и возвращает результат.                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите месяц:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите день: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        string a;
+
+        if ((n < 1) || (n > 12))
+        {
+            a = "Введенно неверное значение";
+        }
+        else
+        {
+            a = "Дата предыдущего дня - " + ds.FindDateOfPreviousDay(m, n);
+        }
+        if ((m < 1) || (m > 32))
+        {
+            a = "Введенно неверное значение";
+        }
+        else
+        {
+            a = "Дата предыдущего дня - " + ds.FindDateOfPreviousDay(m, n);
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(a);
+        Console.ReadKey();
+
+    }
+}
