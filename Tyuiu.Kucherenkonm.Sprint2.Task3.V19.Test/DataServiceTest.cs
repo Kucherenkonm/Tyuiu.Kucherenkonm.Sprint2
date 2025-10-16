6e511665e82a9f6a80f6258ce94568511c8f1c99
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-10-16 10:23:35 +0500

    Task3

---
+++ Tyuiu.Kucherenkonm.Sprint2.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.Kucherenkonm.Sprint2.Task3.V19.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint2.Task3.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 13;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 1.556;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -22;
+            double res = ds.Calculate(x);
+            double wait = 0.956;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -24;
+            double res = ds.Calculate(x);
+            double wait = -263.958;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint2.Task3.V19/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.Kucherenkonm.Sprint2.Task3.V19;
+using Tyuiu.Kucherenkonm.Sprint2.Task3.V19.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема:                   Вложенные операторы if - else                   *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*    Написать программу, которая вычисляет требуемое значение функции Y   *");
+        Console.WriteLine("*   с использованием вложенных оператор if-else, где пользователь вводит  *");
+        Console.WriteLine("*                      переменной X с клавиатуры.                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение х: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = ds.Calculate(x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Значение функции y: " + y);
+        Console.ReadKey();
+
+    }
+}
